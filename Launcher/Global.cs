7afce9417e6015bcd7491f51ae84b839bfa4b21e
Morvin <36500968+M0rv1n@users.noreply.github.com>
2021-04-24 20:10:43 +0530

    Update Global.cs

--- Launcher/Global.cs
+++ Launcher/Global.cs
@@ -10,7 +10,7 @@ namespace Launcher
     {
         public static string _appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
-        public static string _directoryPath = _appData + "\\GameSparkerz";
+        public static string _directoryPath = _appData + "\\CLauncher";
         public static string _nameFile = _directoryPath + "\\names.dat";
         public static string _pathFile = _directoryPath + "\\paths.dat";
     }
